d1bf1aa0d62cb33592941af890a1bfbd11fed1fb
Rafael <>
2018-05-21 21:41:19 +0200

    Rewrote as prototype version (without cheating).

--- NetCoreConsoleTemplate/Program.cs
+++ NetCoreConsoleTemplate/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace JobApplication
 {
@@ -9,30 +11,90 @@ namespace JobApplication
 
         public static void Main()
         {
-            PlayOnConsole();
+            //PlayOnConsole();
+            PlayPrototype();
         }
 
-        private static void PlayOnConsole()
+        //private static void PlayOnConsole()
+        //{
+        //    var game = new Game(wordLength, unknownChar);
+        //    Console.WriteLine("Welcome!");
+        //    while(!game.Guess(GetNewGuess()))
+        //    {
+        //        Console.WriteLine(game.bestCompositeGuess + " untried = " + Helpers.Stringify(game.untriedLetters) + "  to be included = " + Helpers.Stringify(game.wronglyPositionedLetters));
+        //    };
+        //    Console.WriteLine("Right!");
+        //}
+
+        //private static string GetNewGuess()
+        //{
+        //    Console.Write("Enter " + wordLength + "-letter guess: ");
+        //    string newGuess = Console.ReadLine();
+        //    while (newGuess.Length != wordLength)
+        //    {
+        //        Console.Write("Guess has wrong length. Please try again: ");
+        //        newGuess = Console.ReadLine();
+        //    }
+        //    return newGuess.ToUpper();
+        //}
+
+        private static void PlayPrototype()
         {
-            var game = new Game(wordLength, unknownChar);
-            Console.WriteLine("Welcome!");
-            while(!game.Guess(GetNewGuess()))
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            string wordToGuess = GetWordToGuess(alphabet);
+            List<char> wordInProgress = new List<char>(new string(unknownChar, wordLength));
+            string triedLetters = "";
+            string lastGuess = new string(unknownChar, wordLength);
+            //Console.WriteLine(wordToGuess);
+            while (lastGuess != wordToGuess)
             {
-                Console.WriteLine(game.bestCompositeGuess + " untried = " + Helpers.Stringify(game.untriedLetters) + "  to be included = " + Helpers.Stringify(game.wronglyPositionedLetters));
-            };
+                string missingLetters = "";
+                for (int i = 0; i < wordLength; ++i)
+                {
+                    triedLetters += lastGuess[i];
+                    if (wordToGuess[i] == lastGuess[i])
+                    {
+                        wordInProgress[i] = wordToGuess[i];
+                    }
+                    if (wordInProgress[i] == unknownChar)
+                    {
+                        missingLetters += wordToGuess[i];
+                    }
+                }
+                //Console.WriteLine(triedLetters);
+                //Console.WriteLine(missingLetters);
+                Console.WriteLine(Stringify(wordInProgress) + " requiredLetters = " + Stringify(missingLetters.Intersect(triedLetters)) + " untriedLetters = " + Stringify(alphabet.Except(triedLetters)));
+                lastGuess = GetPlayerGuess();
+            }
             Console.WriteLine("Right!");
         }
 
-        private static string GetNewGuess()
+        private static string Stringify(IEnumerable<char> enumerable)
         {
-            Console.Write("Enter " + wordLength + "-letter guess: ");
-            string newGuess = Console.ReadLine();
-            while (newGuess.Length != wordLength)
+            return string.Join("", enumerable);
+        }
+
+        private static string GetPlayerGuess()
+        {
+            string guess = "";
+            while (guess.Length != wordLength)
             {
-                Console.Write("Guess has wrong length. Please try again: ");
-                newGuess = Console.ReadLine();
+                Console.Write("Pick a " + wordLength + " letter word: ");
+                guess = Console.ReadLine().ToUpper();
             }
-            return newGuess.ToUpper();
+            return guess;
+        }
+
+        private static string GetWordToGuess(string alphabet)
+        {
+            Random random = new Random();
+            string wordToGuess = "";
+            for (int i = 0; i < wordLength; ++i)
+            {
+                wordToGuess = wordToGuess + alphabet[random.Next(alphabet.Length)];
+            }
+            return wordToGuess;
         }
     }
 }
