653b7a62ce45bbd712a76a982376595749eb683f
Rafael <>
2018-05-23 11:05:52 +0200

    Figure out keycode events

--- JavaScript/reaction.js
+++ JavaScript/reaction.js
@@ -1,2 +1,16 @@
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 70) { // F-position
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 74) { // J-position
+        alert('Right was pressed');
+    }
+
+})
+	
+function startRound()
+{
 var leftStimulus = document.querySelector('#rightStimulus');
 leftStimulus.textContent = "Bar";
+
+}
