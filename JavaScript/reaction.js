3916710fa76b5bb93203df7d9073407eae01af9c
Rafael <>
2018-05-23 11:36:25 +0200

    Realized that removing event listeners is a pain, changed approach to rely on state instead.

--- JavaScript/reaction.js
+++ JavaScript/reaction.js
@@ -1,16 +1,27 @@
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 70) { // F-position
-        alert('Left was pressed');
-    }
-    else if(event.keyCode == 74) { // J-position
-        alert('Right was pressed');
-    }
+leftCode = 70; // F-position
+rightCode = 74; // J-position
+stimulusCode = undefined;
+var feedback =     document.querySelector('#feedback');
+var leftStimulus = document.querySelector('#leftStimulus');
+var rightStimulus = document.querySelector('#rightStimulus');
+function inputHandler(event) {
+    if(event.keyCode === leftCode || event.keyCode === rightCode) {
+	handleValidKeypress(event.keyCode);
+    } 
+}
 
-})
-	
+function handleValidKeypress(keyCode) {
+    if (stimulusCode === undefined) {
+	feedback.textContent = "Too soon (or too late)";
+    } else if (stimulusCode === keyCode) {
+	feedback.textContent = "Right";
+    } else {
+	feedback.textContent = "Wrong";
+    }
+}
+document.addEventListener('keydown', inputHandler);
+
 function startRound()
 {
-var leftStimulus = document.querySelector('#rightStimulus');
-leftStimulus.textContent = "Bar";
 
 }
